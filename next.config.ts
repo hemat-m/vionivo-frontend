64d0ddfb4d24a7584f5fe74eecb00293a3b80abf
Hemmat Mohammadi <iwpcohemmat@BONIZ-EMAM-5.localdomain>
2026-09-03 14:49:47 +0330

    feat: add Cloudflare OpenNext deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true,
+  },
+  outputFileTracingExcludes: {
+    "*": ["node_modules/next/dist/server/capsize-font-metrics.json"],
+  },
 };
 
 export default nextConfig;
